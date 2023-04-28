9b2ee75cf6436857d720d07b8ff069834b308c25
Aleksy <aslepaugo@gmail.com>
2023-04-28 08:33:15 +0200

    Add logging

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import requests
 import telegram
@@ -9,6 +10,19 @@ from time import sleep
 DVMN_URL = "https://dvmn.org/api/user_reviews/"
 POLLING_URL = "https://dvmn.org/api/long_polling/"
 TIMEOUT = 120
+logger = logging.getLogger("dvmn-review-bot")
+logger.setLevel(os.environ.get("LOGGING_LEVEL", "INFO"))
+
+
+class TelegramLogsHandler(logging.Handler):
+    def __init__(self, tg_bot: telegram.Bot, chat_id: int) -> None:
+        super().__init__()
+        self.tg_bot = tg_bot
+        self.chat_id = chat_id
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def poll_for_new_reviews():
@@ -19,7 +33,10 @@ def poll_for_new_reviews():
         "timestamp": None
     }
     bot = telegram.Bot(token=os.environ['TELEGRAM_TOKEN'])
-    chat_id = os.environ['TELEGRAM_USER_ID']    
+    chat_id = os.environ['TELEGRAM_USER_ID']
+
+    logger.addHandler(TelegramLogsHandler(bot, chat_id))
+    logger.info("Бот запущен")
     while True:
         try:
             response = requests.get(POLLING_URL, headers=headers, timeout=TIMEOUT, params=params)
@@ -29,9 +46,12 @@ def poll_for_new_reviews():
                 params = {
                     "timestamp": review_response['timestamp_to_request']
                 }
+                logger.info("Бот ожидает новых ревью")
             elif review_response['status'] == 'found':
+                logger.info("Бот получил новые ревью")
                 for attempt in review_response['new_attempts']:
                     if attempt['is_negative']:
+                        logger.info("Бот - преподаватель нашел ошибки в работе")
                         bot.send_message(
                             text=f'Преподаватель нашел ошибки в работе: \
                                    <a href="{attempt["lesson_url"]}">{attempt["lesson_title"]}</a>',
@@ -39,6 +59,7 @@ def poll_for_new_reviews():
                             parse_mode='HTML'
                             )
                     else:
+                        logger.info("Бот - преподаватель проверил работу")
                         bot.send_message(
                             text=f'Преподаватель проверил работу: \
                                 <a href="{attempt["lesson_url"]}">{attempt["lesson_title"]}</a>',
@@ -49,8 +70,10 @@ def poll_for_new_reviews():
                     "timestamp": review_response['last_attempt_timestamp']
                 }
         except requests.exceptions.ReadTimeout:
+            logger.warning("Бот - превышен таймаут")
             continue
         except requests.exceptions.ConnectionError:
+            logger.warning("Бот - ошибка соединения")
             sleep(5)
             continue
 
